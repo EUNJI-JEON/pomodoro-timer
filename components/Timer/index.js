0328a2c9f8e016b9e247b4fe76e2c46bf388c747
eunjijeon <jej.career@gmail.com>
2019-05-07 12:01:58 +0900

    created some components for app

---
+++ components/Timer/index.js
@@ -0,0 +1,47 @@
+import React, {Component} from "react";
+import {View,Text,StyleSheet, StatusBar} from "react-native";
+import Button from "../Button";
+
+
+class Timer extends Component{
+    render(){
+        return(
+            <View style={styles.container}>
+                <StatusBar barStyle={"light-content"}></StatusBar>
+                <View style={styles.upper}>
+                    <Text style={styles.time}>24:00</Text>
+                </View>
+                <View style={styles.lower}>
+                    <Button iconName="play-circle"></Button>
+                    <Button iconName="stop-circle"></Button>
+                </View>
+            </View>
+        )
+    }
+}
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor:"#f78fb3"
+    },
+    upper: {
+        flex:2,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    lower: {
+        flex:1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    time: {
+        color: "white",
+        fontSize: 120,
+        fontWeight: "100"
+    }
+});
+
+export default Timer;
